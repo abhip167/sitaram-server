e140f7bc9206133e0f7eb122403e0b14ef4b0a52
Abhishek Patel <abhishekpatel@pop-os.localdomain>
2020-07-19 20:12:18 +0530

    Deployed GraphQL Server

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import jsonGraphqlExpress from "json-graphql-server";
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import data from "./result";
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/graphql", jsonGraphqlExpress(data));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 export default app;
