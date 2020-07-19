ac4afedb0798b79aa105779a1a3ee4fe7f0d8e98
Abhishek Patel <abhishekpatel@pop-os.localdomain>
2020-07-19 21:18:08 +0530

    Changed

--- server/app.js
+++ server/app.js
@@ -1,23 +1,20 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import jsonGraphqlExpress from "json-graphql-server";
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
-import data from "./result";
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("/graphql", jsonGraphqlExpress(data));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 export default app;
